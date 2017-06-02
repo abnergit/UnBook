aa61230fa2e28d6cb8518b5546880072af75ff65
abnergit <denysabner@gmail.com>
2017-06-01 23:09:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -222,7 +222,8 @@ switch (ENVIRONMENT)
 	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
 	// Path to the system folder
-	define('BASEPATH', str_replace('\\', '/', $system_path));
+	//define('BASEPATH', str_replace('\\', '/', $system_path));
+	  define('BASEPATH', "/app/");
 
 	// Path to the front controller (this file)
 	define('FCPATH', dirname(__FILE__).'/');
